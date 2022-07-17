298f2e4c940da2b811117eb526826447c805b865
emilianscheel <emilian-scheel@t-online.de>
2022-07-17 13:05:36 +0200

    init

---
+++ main.py
@@ -0,0 +1,33 @@
+import urllib3
+from datetime import datetime
+import json
+
+
+dataFolder = "data/"
+
+
+def fetch():
+
+    apiURL = "https://www.tagesschau.de/api2/"
+
+    dateTimeObj = datetime.now()
+    timestamp = dateTimeObj.strftime(
+        "%d-%m-%Y--%H-%M-%S")
+
+    http = urllib3.PoolManager()
+    request = http.request('GET', apiURL)
+    if (request.status != 200):
+        return
+    jsonData = json.loads(request.data.decode('utf-8'))
+
+    filename = timestamp + ".json"
+
+    open(dataFolder + filename, "x")
+
+    file = open(dataFolder + filename, "w")
+    file.write(json.dumps(jsonData))
+    file.close()
+
+
+if __name__ == "__main__":
+    fetch()
